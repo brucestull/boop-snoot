8048afbd432d054ae0777aaa1896dce0aa0725d7
FlynntKnapp <bruce.stull@gmail.com>
2023-10-12 01:51:46 -0400

    Override 'get_changeform_initial_data' method.

--- boops/admin.py
+++ boops/admin.py
@@ -6,3 +6,19 @@ from .models import Boop
 class BoopAdmin(admin.ModelAdmin):
     list_display = ['fuzzy_one', 'order']
     list_editable = ['order']
+
+    def get_changeform_initial_data(self, request):
+        """
+        Override the default initial data to set the order to the highest
+        order number + 1
+        """
+        # Get the initial data from the default method
+        initial_data = super().get_changeform_initial_data(request)
+        # Get the highest order number
+        highest_order = Boop.objects.all().aggregate(
+            models.Max('order'))['order__max']
+        # Set the order to the highest order number + 1
+        initial_data['order'] = (
+            highest_order if highest_order is not None else -1) + 1
+        # Return the initial data
+        return initial_data
