7134484f564bb7c82088ccf909c065b8c62b485e
FlynntKnapp <bruce.stull@gmail.com>
2023-10-12 01:54:58 -0400

    Used 'isort' to sort imports

--- boops/views.py
+++ boops/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, redirect, render
+
 from .models import Boop
-from django.shortcuts import get_object_or_404
 
 
 def boop_list_view(request):
