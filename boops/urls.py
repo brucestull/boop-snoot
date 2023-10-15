69dd1f929afd01e1f959ac632721ab684edf12f5
FlynntKnapp <bruce.stull@gmail.com>
2023-10-15 19:54:20 -0400

    Revert "Big changes"
    This reverts commit cce1855bcb89fa9eadc2986e3e4c78585f26060d.

--- boops/urls.py
+++ boops/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.boop_list_view, name="boop_list"),
-    path("<int:boop_id>/move-up/", views.move_up, name="move_up"),
-    path("<int:boop_id>/delete/", views.delete_boop, name="delete_boop"),
+    path('', views.boop_list_view, name='boop_list'),
+    path('<int:boop_id>/move-up/', views.move_up, name='move_up'),
+    path('<int:boop_id>/delete/', views.delete_boop, name='delete_boop'),
 ]
--- boops/views.py
+++ boops/views.py
@@ -1,26 +1,11 @@
 from django.shortcuts import get_object_or_404, redirect, render
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
 
 from .models import Boop
-from .forms import BoopForm
 
 
 def boop_list_view(request):
     boops = Boop.objects.all()
-    form = BoopForm()
-
-    # Check if form is submitted
-    if request.method == "POST":
-        form = BoopForm(request.POST)
-        if form.is_valid():
-            new_boop = form.save(commit=False)
-            new_boop.save()
-            return redirect(
-                "boop_list"
-            )  # assuming 'boop_list' is the name of the URL pattern for this view
-
-    return render(request, "boop_list.html", {"boops": boops, "form": form})
+    return render(request, 'boop_list.html', {'boops': boops})
 
 
 def move_up(request, boop_id):
@@ -33,11 +18,11 @@ def move_up(request, boop_id):
         previous_boop.save()
         Boop.reorder_all()
 
-    return redirect("boop_list")
+    return redirect('boop_list')
 
 
 def delete_boop(request, boop_id):
     boop = get_object_or_404(Boop, id=boop_id)
     boop.delete()
     Boop.reorder_all()
-    return redirect("boop_list")
+    return redirect('boop_list')
